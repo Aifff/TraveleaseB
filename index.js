9f5dcc8eb3380f1f3d27eaa8384ec59dd6351055
Mohammed Arif <122438883+Aifff@users.noreply.github.com>
2024-03-28 01:19:28 +0530

    Update index.js CORS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors({
   // origin: 'http://localhost:3000',
-  origin: 'https://travelease-f.vercel.app',
+  origin: 'https://travelease-f-blue.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
   })
