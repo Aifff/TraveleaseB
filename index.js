7c35d4d7713fe79712eaaee6d6397ca5cb05620f
Mohammed Arif <abdulraheemarif10@gmail.com>
2024-03-28 00:49:19 +0530

    cors changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors({
   // origin: 'http://localhost:3000',
-  origin: 'https://travelease-indol.vercel.app/',
+  origin: 'https://travelease-f.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
   })
